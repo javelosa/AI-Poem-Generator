361ecaca9287955e3028703c44464801d392e257
javelosa <49219594+javelosa@users.noreply.github.com>
2025-07-25 11:31:56 +0800

    Fixed js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ function makePoem(event) {
   new Typewriter("#poem-content", {
     strings: ["To be or not to be."],
     autoStart: true,
-    delay: 1,
-    cursor: null,
+    delay: 10,
+    cursor: "",
   });
-  poemGenerator.innherHTML = "To be or not to be.";
 }
 
 let poemFormElement = document.querySelector("#poem-creator");
